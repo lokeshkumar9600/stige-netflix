18642690ea782f0bc2ca2e1fb566b001a7d959d1
lokeshkumar9600 <lokesh960077152@gmail.com>
2022-02-27 17:19:32 +0530

    Updates

---
+++ precache-manifest.5379b35e3b9a4e74eaa0b06bc5fd354e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893f11c274fea65e32b2c7ebac873d22",
+    "url": "/stige-netflix/index.html"
+  },
+  {
+    "revision": "3c7d7479a2fe8bf86a9d",
+    "url": "/stige-netflix/static/css/main.020e2537.chunk.css"
+  },
+  {
+    "revision": "14ce56e29231dac91901",
+    "url": "/stige-netflix/static/js/2.066fe1f8.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/stige-netflix/static/js/2.066fe1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7d7479a2fe8bf86a9d",
+    "url": "/stige-netflix/static/js/main.1c5b436e.chunk.js"
+  },
+  {
+    "revision": "36a8293f0ab94a464759",
+    "url": "/stige-netflix/static/js/runtime-main.bf0fa085.js"
+  }
+]);
